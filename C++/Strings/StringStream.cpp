8dd61ef2aa0a62f2638c0f8b81212a2eb1c25672
Mahfuz Ahmed <likhon.codew@gmail.com>
2019-08-17 16:21:33 +0600

    solve StringStream

---
+++ C++/Strings/StringStream.cpp
@@ -0,0 +1,32 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	stringstream ss(str);
+    char ch;
+    vector<int> vec;
+    int a;
+
+    ss >> a;
+    vec.push_back(a);
+
+    while(ss >> ch >> a){
+        vec.push_back(a);
+    }
+
+    return vec;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
+
